b8c7dd4c47caf873dfd15e9d13452813ea08ccd7
Arshamirali37 <arshamirali37@gmail.com>
2024-08-25 17:05:52 +0500

    Code update

--- 18-main.dart
+++ 18-main.dart
@@ -2,8 +2,8 @@ void main (){
   var email = "abc@gmail.com";
   var password = "123456";
 
-  if (email && password){
-    print("Login");
+  if (email == "abc@gmail.com" && password == "123456"){
+    print("Login Successfully");
   }else {
     print("Not Login");
   }
